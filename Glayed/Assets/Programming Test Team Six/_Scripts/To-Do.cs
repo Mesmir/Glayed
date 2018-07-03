1ecd611c6d103894e77c87755a1bd2e1f3db6414
Renzo-PC\Renzo <eagyr@hotmail.nl>
2018-07-03 13:29:18 +0200

    sssht

--- Glayed/Assets/Programming Test Team Six/_Scripts/To-Do.cs
+++ Glayed/Assets/Programming Test Team Six/_Scripts/To-Do.cs
@@ -1,13 +1,64 @@
-﻿/*
+﻿/* Concepting;
+ * Idea; tower type, needs x amount of kils to be able to unlock/upgrade
+ * Idea; At later level be able to get 2 tower simultaneously, or combine 2 of them?
+ * Idea; New Game +
+ * Idea; Secret Based Unlocking
+ * Idea; Utility Buildings use Electricity
+ * Idea; Alignments (like Terror, Human, Nature (I.E, Upgrading alot of Gore-y stuff (Panic, Fear, Blood stuff), increases your Terror Allignment, giving you buffs and debuffs accordingly (Perhaps even change the base))
+ * Hexagonial Pillar Floor 
  * 
- * // Turret movement/LookAt Mouse (360)
- * /Gun can fire and do dmg to enemy's
- * Gun Hotswapping.
- * Different Guntypes
+ * Currency (+interest) / ScoreBased, WaveBased unlocking?
  * 
- * Bonus; A.I Controlled Turret
- * Moving Enemy's
- * Score
- * Button Controlled turret (??)
+ * Shop(Technology Center) ~Instead of instant purchase, you "Research" every unlock, which goes over time(or wave)
+ *  - Turret Upgrades (Fire Rate, Dmg, Ammo, Reload Time, Turret Specific 1, Turret Specific 2)
+ *  - Ammo type (Extra Effects, I.e. Elemental types?)
+ * 
+ * Turrets; 
+ *  - Standard, Rapid Fire (Machine gun), Pierce (Lazer), Sniper (Slow, Single Target, Huge dmg), Rocket Launcher (Splash), Glaive (Bounce), Boomerang(return Bullit), Trap Layer (lays traps in a spot or area), 
+ *    Curse (places a curse on the enemy unit, increasing equipped bullit effect dramatically), Orbitter (A sphere orbit's the base), Gate (Probius ult), 
+ * 
+ * Ammo Types; ~Ammo Equip Cap (10), each ammo type costs 1 (+1 per upgraded lvl), can arrange self.- Depending on turret, not every bullit can be equipped
+ *  - Fire(AoE Dot), Poison(Heavy single target Dot), Sharp/Slash(Bleed Dot?), Ice(Slow), Panic, Fear, Knockback, Lightning Strike, Swipe(kan ook turret zijn)
+ * 
+ * Utility Buildings only; 
+ *  - A.I Turret (Any turret that has been unlocked (@50% efficiency (Upgradeable?))
+ *  - Research Center(speeds up research time/allows certain researches, or Reduces Research cost by x% & 1 more for every kill made)
+ *  - Radar Bot (Lights up the closest or most dangerous enemy's, also grants bonus dmg to them)
+ *  - Decoy Bot (Let's you place a decoy somewhere which will attract the enemy, higher levels will allow you to give an effect to the decoy, i.e. a basic gun, or slow)
+ *  - Reinforcements (Deploys drones to fight and defend the base)
+ *  - Repair Bot (Repairs the base)
+ *  - Interest Rate (Increases the interest rate at the end of the wave, high levels also gives enemy's a chance to give extra gold / Banking option?)
+ * 
+ * Upgrading; 
+ *  - Every upgrade needs to be researched which takes (game)time or x amount of waves
+ *  - Each turret Level increases ammo cap by 1 (A turret gains a Level every 5 upgrades)
+ *  - Each Ammo upgrade increases its potency
+ *  - Cost rises exponentially
+ * 
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * 
+ * Assets
+ * Needs:
+ * A.I Turret
+ * Shop Menu: (Technology Research Center)
+ *  - New Turret Research Purchases
+ *  - Turret Research Upgrades
+ *  - UtilitySlot Buildings management (Research/Sell/Upgrade)
+ *  - Ammo type
+ *  
+ *  
+ * Currency (+interest) / ScoreBased unlocking?
+ * 5 towers (Standard, Machine, Lazer, Rocket launcher, Glaive, Boomerang)
+ * Actual Waves
+ * 
+ * Wants: 
+ * 5 extra towers
+ * 
+ * Multiple unit types
+ * 
+ * Extra's: 
+ * 
+ * Bonus: 
+ *  - A.I Controlled Turret
  * 
  */
